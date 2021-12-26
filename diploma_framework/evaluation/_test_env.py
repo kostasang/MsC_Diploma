981d63f0ff342bde22e8961e48cca8176cffc37f
kostasang <kostas.ang97@gmail.com>
2021-12-26 14:49:43 +0200

    Add test_env function

---
+++ diploma_framework/evaluation/_test_env.py
@@ -0,0 +1,32 @@
+from typing import Union
+from gym_duckietown import DuckietownEnv
+
+import torch, gym
+import torch.nn as nn
+
+
+def test_env(env : Union[DuckietownEnv, str],
+             model : nn.Module, vis : bool = False) -> float :
+
+    """
+    Used for testing the so far trained model. It calculates the total reward 
+    received when model operates on the environment.
+
+    """
+
+    if isinstance(env, str):
+        env = gym.make(env)
+    state = env.reset()
+    if vis: env.render()
+    done = False
+    total_reward = 0
+    while not done:
+        state = torch.FloatTensor(state).unsqueeze(0)
+        dist, _ = model(state)
+        dist = torch.distributions.Categorical(logits=dist)
+        next_state, reward, done, _ = env.step(dist.sample().cpu().numpy()[0])
+        state = next_state
+        if vis: env.render()
+        total_reward += reward
+    env.close()
+    return total_reward
