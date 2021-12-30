f8ed50c6a2d3cb55dfa007daf47e9d592f1f2b71
kostasang <kostas.ang97@gmail.com>
2021-12-31 01:26:25 +0200

    Add reward_threshold parameter to run method

--- diploma_framework/algorithms/_deep_q_learning.py
+++ diploma_framework/algorithms/_deep_q_learning.py
@@ -50,7 +50,8 @@ class DQN():
     def run(self, 
             eval_window : int = 1000,
             n_evaluations : int = 10,
-            early_stopping : bool = True) -> list:
+            early_stopping : bool = True,
+            reward_threshold : float = 197.5) -> list:
         
         """
         Run the DQN algorithm with hyperparameters specified in arguments.
@@ -118,7 +119,7 @@ class DQN():
                     test_reward = np.mean([test_env(self.env, self.model, vis=False) for _ in range(n_evaluations)])
                     test_rewards.append(test_reward)
                     print(f'Frame : {frame_idx} - Test reward : {test_reward}')
-                    if test_reward > 195.2 and early_stopping: early_stop = True
+                    if test_reward > reward_threshold and early_stopping: early_stop = True
 
         return test_rewards
 
