6752f7c9293623c857fbf8da1dd52d69c052c3e3
kostasang <kostas.ang97@gmail.com>
2022-01-01 03:34:38 +0200

    Add logging functionality

--- diploma_framework/algorithms/__init__.py
+++ diploma_framework/algorithms/__init__.py
@@ -1,4 +1,14 @@
 from ._ppo import PPO
 from ._deep_q_learning import DQN
 from ._reinforce import Reinforce
-from ._a3c import A3C 
+from ._a3c import A3C
+
+import logging
+
+logger = logging.getLogger("deepRL")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+ch.setLevel(logging.INFO)
+logger.addHandler(ch)
