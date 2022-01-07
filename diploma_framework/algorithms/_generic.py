dd66446282147e2a903f150870e0c0f45105eeb4
kostasang <kostas.ang97@gmail.com>
2022-01-07 14:26:48 +0200

    Create base class for every algorithm

---
+++ diploma_framework/algorithms/_generic.py
@@ -0,0 +1,29 @@
+import joblib, torch
+
+class DeepRLAlgorithm():
+
+    """
+    Implements basic methods that every algorithm in the framework will inherit
+
+    """
+
+    def get_model(self) -> torch.nn.Module :
+        
+        """
+        Returns th algorithms neural network model
+
+        """
+        return self.model 
+
+    def save_model(self, path : str):
+
+        """
+        Saves the algorithm's model to the specified path
+
+        """
+        try:
+            joblib.dump(self.model, path)
+        except:
+            raise Exception(f'Poblem storing model to {path}')
+
+        
