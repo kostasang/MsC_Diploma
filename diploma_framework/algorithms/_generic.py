142e1176cf812c68a332cae68a1e2e2333d04ae5
kanagnostopoulos <kanagnostopoulos@github.com>
2022-01-19 22:32:31 +0200

    Add evaluation method to parent class

--- diploma_framework/algorithms/_generic.py
+++ diploma_framework/algorithms/_generic.py
@@ -1,4 +1,6 @@
-import joblib, torch
+from typing import Tuple
+from diploma_framework.evaluation import test_env
+import joblib, torch, numpy as np
 
 class DeepRLAlgorithm():
 
@@ -26,4 +28,16 @@ class DeepRLAlgorithm():
         except:
             raise Exception(f'Poblem storing model to {path}')
 
+    def evaluate(self, n_evaluations : int) -> Tuple[float, float] :
+
+        """
+        Evaluate model on environment n_evaluations times and 
+        extract average metrics
+
+        """
+
+        evaluations = [test_env(self.env, self.model, vis=False) for _ in range(n_evaluations)]
+        average_reward = np.mean([metric[0] for metric in evaluations])
+        average_nframes = np.mean([metric[1] for metric in evaluations])
         
+        return average_reward, average_nframes
