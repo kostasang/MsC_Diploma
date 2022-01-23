387a62e0235a68e89051a0fdaff74d629a86a31a
kostasang <kostas.ang97@gmail.com>
2022-01-23 02:52:27 +0200

    Add custom evaluation functionality

--- diploma_framework/algorithms/_generic.py
+++ diploma_framework/algorithms/_generic.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Callable, Tuple, Union
 from diploma_framework.evaluation import test_env
 import joblib, torch, numpy as np
 
@@ -28,15 +28,22 @@ class DeepRLAlgorithm():
         except:
             raise Exception(f'Poblem storing model to {path}')
 
-    def evaluate(self, n_evaluations : int) -> Tuple[float, float] :
+    def evaluate(self,
+                 n_evaluations : int,
+                 custom_test : Union[Callable,None] = None) -> Tuple[float, float] :
 
         """
         Evaluate model on environment n_evaluations times and 
-        extract average metrics
+        extract average metrics. Evaluation function can be the predifined one
+        or a custom user defined.
 
         """
 
-        evaluations = [test_env(self.env, self.model, vis=False) for _ in range(n_evaluations)]
+        if custom_test is None:
+            evaluations = [test_env(self.env, self.model, vis=False) for _ in range(n_evaluations)]
+        else:
+            evaluations = [custom_test(self.env, self.model, vis=False) for _ in range(n_evaluations)]
+
         average_reward = np.mean([metric[0] for metric in evaluations])
         average_nframes = np.mean([metric[1] for metric in evaluations])
         
