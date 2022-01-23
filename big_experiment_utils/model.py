ec59472828f8a5449a5cae94f6ec9d932fe76d7b
kostasang <kostas.ang97@gmail.com>
2022-01-23 21:19:57 +0200

    Add small experiments folder

---
+++ big_experiment_utils/model.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms as T
+from torch.nn import functional as F 
+
+class CNNActorCritic(nn.Module):
+
+    def __init__(self, n_output, device):
+
+        super(CNNActorCritic, self).__init__()
+
+        self.device = device
+        self.transform = T.Compose([
+                    T.Normalize(mean=[0]*15, std=[255]*15),   # Turn input to 0-1 range from 0-255
+                    ])
+
+        self.conv_core = nn.Sequential(
+            nn.Conv2d(15, 32, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 32, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(),
+            nn.Dropout(0.3)
+        ).to(self.device)
+        
+        self.actor_head = nn.Sequential(
+            nn.Linear(2560,128),
+            nn.ReLU(),
+            nn.Linear(128, n_output)
+        ).to(device=self.device)
+
+        self.critic_head = nn.Sequential(
+            nn.Linear(2560, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        ).to(device=self.device)
+        self.apply(self.init_weights)
+
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, mean=0., std=0.1)
+            nn.init.constant_(m.bias, 0.1)
+
+    def forward(self, x):
+        x = torch.permute(x, (0, 3, 1, 2))  # Place channel axis in correct position
+        x = self.transform(x)               # Apply transform
+        x = x.to(device=self.device)
+        visual_repr = self.conv_core(x).squeeze(-1).squeeze(-1)   # Calculate ResNet output
+        return F.log_softmax(self.actor_head(visual_repr), dim=1).to('cpu'), self.critic_head(visual_repr.detach()).to('cpu') # Calculate policy probs and value
+
+    def infer_step(self, x):
+        action_probs, value = self.forward(x)
+        dist = torch.distributions.Categorical(logits=action_probs)
+        action = dist.sample().item()
+        return dist, action, value 
+
+    def infer_batch(self, x):
+        action_probs, value = self.forward(x)
+        dist = torch.distributions.Categorical(logits=action_probs)
+        return dist, value
+
+    def infer_action(self, x):
+        dist, _ = self.forward(x)
+        dist = torch.distributions.Categorical(logits=dist)
+        return dist.sample().cpu().numpy()[0]
