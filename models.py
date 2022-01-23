ed1fd5a9eef9d5319d86638016f590a22b4adb60
kostasang <kostas.ang97@gmail.com>
2022-01-23 02:53:02 +0200

    Change model architecture

--- models.py
+++ models.py
@@ -5,63 +5,6 @@ from torchvision import transforms as T
 from torch.nn import functional as F 
 from PIL import Image
 
-
-class EnhancedActorCritic(nn.Module):
-    
-    def __init__(self, n_output, normalization_means, normalization_stds, device):
-        
-        super(EnhancedActorCritic,self).__init__()
-        
-        self.device = device
-        model = models.resnet18(pretrained=True, progress=True)
-        self.transform = T.Compose([
-                    T.Normalize(mean=[0,0,0], std=[255,255,255]),   # Turn input to 0-1 range from 0-255
-                    T.Normalize(mean=normalization_means, std=normalization_stds),  # Normalization for ResNet
-                    T.Resize(256),
-                    T.CenterCrop(224)])
-
-        
-        self.resnet_core = nn.Sequential(*(list(model.children())[:-1])).to(device=self.device)
-
-        # Freeze resnet parameters (transfer learning approach)
-        for child in self.resnet_core.children():
-            for parameter in child.parameters():
-                parameter.requires_grad = True
-        
-        self.actor_head = nn.Sequential(
-            nn.Linear(512,256),
-            nn.ReLU(),
-            nn.Linear(256, n_output)
-        ).to(device=self.device)
-        self.critic_head = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 1)
-        ).to(device=self.device)
-
-    def forward(self, x):
-        x = torch.permute(x, (0, 3, 1, 2))  # Place channel axis in correct position
-        x = self.transform(x)               # Apply transform
-        x = x.to(device=self.device)
-        visual_repr = self.resnet_core(x).squeeze(-1).squeeze(-1)   # Calculate ResNet output
-        return F.log_softmax(self.actor_head(visual_repr), dim=1).to('cpu'), self.critic_head(visual_repr).to('cpu') # Calculate policy probs and value
-
-    def infer_step(self, x):
-        action_probs, value = self.forward(x)
-        dist = torch.distributions.Categorical(logits=action_probs)
-        action = dist.sample().item()
-        return dist, action, value 
-    
-    def infer_batch(self, x):
-        action_probs, value = self.forward(x)
-        dist = torch.distributions.Categorical(logits=action_probs)
-        return dist, value
-
-    def infer_action(self, x):
-        dist, _ = self.forward(x)
-        dist = torch.distributions.Categorical(logits=dist)
-        return dist.sample().cpu().numpy()[0]
-
 class CNNActorCritic(nn.Module):
 
     def __init__(self, n_output, device):
@@ -70,37 +13,33 @@ class CNNActorCritic(nn.Module):
 
         self.device = device
         self.transform = T.Compose([
-                    T.Normalize(mean=[0,0,0], std=[255,255,255]),   # Turn input to 0-1 range from 0-255
-                    T.Resize(256),
-                    T.CenterCrop(224)])
+                    T.Normalize(mean=[0]*15, std=[255]*15),   # Turn input to 0-1 range from 0-255
+                    ])
 
         self.conv_core = nn.Sequential(
-            nn.Conv2d(3, 32, 8, 2),
+            nn.Conv2d(15, 32, 3),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, 8, 2),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 32, 3),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 16, 8, 2),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, 3),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 8, 8, 2),
-            nn.ReLU(),
-            nn.BatchNorm2d(8),
+            nn.MaxPool2d(2, 2),
             nn.Flatten(),
             nn.Dropout(0.3)
         ).to(self.device)
         
         self.actor_head = nn.Sequential(
-            nn.Linear(512,256),
+            nn.Linear(2560,128),
             nn.ReLU(),
-            nn.Linear(256, n_output)
+            nn.Linear(128, n_output)
         ).to(device=self.device)
 
         self.critic_head = nn.Sequential(
-            nn.Linear(512, 256),
+            nn.Linear(2560, 128),
             nn.ReLU(),
-            nn.Linear(256, 1)
+            nn.Linear(128, 1)
         ).to(device=self.device)
         self.apply(self.init_weights)
 
