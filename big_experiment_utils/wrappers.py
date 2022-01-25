beaaf45fc0ca480cd345f0002cf34a0f125cce04
kostasang <kostas.ang97@gmail.com>
2022-01-25 15:02:07 +0200

    Add discrete action space wrapper

--- big_experiment_utils/wrappers.py
+++ big_experiment_utils/wrappers.py
@@ -1,89 +1,6 @@
-import re
 import gym
 from gym import spaces
 import numpy as np
-
-from gym_duckietown.simulator import Simulator
-
-
-class MotionBlurWrapper(Simulator):
-    def __init__(self, env=None):
-        Simulator.__init__(self)
-        self.env = env
-        self.frame_skip = 3
-        self.env.delta_time = self.env.delta_time / self.frame_skip
-
-    def step(self, action: np.ndarray):
-        action = np.clip(action, -1, 1)
-        # Actions could be a Python list
-        action = np.array(action)
-        motion_blur_window = []
-        for _ in range(self.frame_skip):
-            obs = self.env.render_obs()
-            motion_blur_window.append(obs)
-            self.env.update_physics(action)
-
-        # Generate the current camera image
-
-        obs = self.env.render_obs()
-        motion_blur_window.append(obs)
-        obs = np.average(motion_blur_window, axis=0, weights=[0.8, 0.15, 0.04, 0.01])
-
-        misc = self.env.get_agent_info()
-
-        d = self.env._compute_done_reward()
-        misc["Simulator"]["msg"] = d.done_why
-
-        return obs, d.reward, d.done, misc
-
-
-class ResizeWrapper(gym.ObservationWrapper):
-    def __init__(self, env=None, shape=(120, 160, 3)):
-        super(ResizeWrapper, self).__init__(env)
-        self.observation_space.shape = shape
-        self.observation_space = spaces.Box(
-            self.observation_space.low[0, 0, 0],
-            self.observation_space.high[0, 0, 0],
-            shape,
-            dtype=self.observation_space.dtype,
-        )
-        self.shape = shape
-
-    def observation(self, observation):
-        from scipy.misc import imresize
-
-        return imresize(observation, self.shape)
-
-
-class NormalizeWrapper(gym.ObservationWrapper):
-    def __init__(self, env=None):
-        super(NormalizeWrapper, self).__init__(env)
-        self.obs_lo = self.observation_space.low[0, 0, 0]
-        self.obs_hi = self.observation_space.high[0, 0, 0]
-        obs_shape = self.observation_space.shape
-        self.observation_space = spaces.Box(0.0, 1.0, obs_shape, dtype=np.float32)
-
-    def observation(self, obs):
-        if self.obs_lo == 0.0 and self.obs_hi == 1.0:
-            return obs
-        else:
-            return (obs - self.obs_lo) / (self.obs_hi - self.obs_lo)
-
-
-class ImgWrapper(gym.ObservationWrapper):
-    def __init__(self, env=None):
-        super(ImgWrapper, self).__init__(env)
-        obs_shape = self.observation_space.shape
-        self.observation_space = spaces.Box(
-            self.observation_space.low[0, 0, 0],
-            self.observation_space.high[0, 0, 0],
-            [obs_shape[2], obs_shape[0], obs_shape[1]],
-            dtype=self.observation_space.dtype,
-        )
-
-    def observation(self, observation):
-        return observation.transpose(2, 0, 1)
-
 
 class DtRewardWrapper(gym.RewardWrapper):
     def __init__(self, env):
@@ -93,17 +10,34 @@ class DtRewardWrapper(gym.RewardWrapper):
         if reward == -1000:
             reward = -10
         elif reward > 0:
-            reward += 10
+            pass
         else:
-            reward += 4
+            reward = 0
         return reward
 
+class DiscreteWrapper(gym.ActionWrapper):
+    """
+    Duckietown environment with discrete actions (left, right, forward)
+    instead of continuous control
+    """
 
-# this is needed because at max speed the duckie can't turn anymore
-class ActionWrapper(gym.ActionWrapper):
     def __init__(self, env):
-        super(ActionWrapper, self).__init__(env)
+        gym.ActionWrapper.__init__(self, env)
+        self.action_space = spaces.Discrete(3)
 
     def action(self, action):
-        action_ = [action[0] * 0.8, action[1]]
-        return action_
+        # Turn left
+        if action == 0:
+            vels = [-0.6, 0.6]
+        # Turn right
+        elif action == 1:
+            vels = [0.6, -0.6]
+        # Go forward
+        elif action == 2:
+            vels = [0.7, 0.7]
+        else:
+            assert False, "unknown action"
+        return np.array(vels)
+
+    def reverse_action(self, action):
+        raise NotImplementedError()
