a21bba3cafe5f8ef6e01bbed145c313b1acb6836
kostasang <kostas.ang97@gmail.com>
2022-03-05 19:49:07 +0200

    Add test for new map

--- big_experiment_utils/utils.py
+++ big_experiment_utils/utils.py
@@ -12,10 +12,49 @@ def test_duckietown(env : Union[object, str],
                     vis : bool = False,
                     stacked_frames : int = 5) -> float :
     
-    
     env = Simulator(
         seed=None,  # random seed
         map_name="loop_empty",
+        max_steps=3_500,  # we don't want the gym to reset itself
+        domain_rand=False,
+        distortion=False,
+        camera_width=80,
+        camera_height=60,
+        draw_curve=True,
+        accept_start_angle_deg=4
+    )
+    env = DtRewardWrapper(env)
+    env = DiscreteWrapper(env)
+
+    frame = env.reset()
+    stacked_frames = deque([torch.zeros(size=frame.shape).unsqueeze(0)]*stacked_frames,
+                           maxlen=stacked_frames)
+
+    if vis: env.render()
+    done = False
+    total_reward = 0
+    total_frames = 0
+    while not done:
+        frame = torch.FloatTensor(frame).unsqueeze(0)
+        stacked_frames.append(frame)
+        state = torch.cat(tuple(stacked_frames), dim=-1)
+        action = model.infer_action(state)
+        next_frame, reward, done, _ = env.step(action)
+        frame = next_frame
+        if vis: env.render()
+        total_reward += reward
+        total_frames += 1
+    env.close()
+    return total_reward, total_frames
+
+def test_duckietown_pedestrians(env : Union[object, str],
+                    model : nn.Module,
+                    vis : bool = False,
+                    stacked_frames : int = 5) -> float :
+    
+    env = Simulator(
+        seed=None,  # random seed
+        map_name="loop_pedestrians",
         max_steps=3_500,  # we don't want the gym to reset itself
         domain_rand=False,
         distortion=False,
